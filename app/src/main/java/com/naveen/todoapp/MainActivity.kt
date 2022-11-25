b58e37f4f97bb27da97fd7f415e3e847b71b7c2d
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-25 17:04:14 +0530

    Setup ActionBar With NavigationController

--- app/src/main/java/com/naveen/todoapp/MainActivity.kt
+++ app/src/main/java/com/naveen/todoapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.naveen.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.naveen.todoapp.fragments.list.ListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/naveen/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/naveen/todoapp/fragments/list/ListFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.naveen.todoapp.R
 
 class ListFragment : Fragment() {
+
+     lateinit var button : FloatingActionButton
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        button = view.findViewById(R.id.floatingActionButton)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment )
+        }
+
+        return view
     }
 }
